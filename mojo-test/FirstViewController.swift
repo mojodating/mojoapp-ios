caaf186fa78d275227c3cf6c701d799a817150ce
Yunyun Chen <yunyun.chen@consensys.net>
2018-11-23 15:37:26 -0500

    Initial Commit

---
+++ mojo-test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  mojo-test
+//
+//  Created by Yunyun Chen on 11/23/18.
+//  Copyright © 2018 Yunyun Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ mojo-testTests/mojo_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  mojo_testTests.swift
+//  mojo-testTests
+//
+//  Created by Yunyun Chen on 11/23/18.
+//  Copyright © 2018 Yunyun Chen. All rights reserved.
+//
+
+import XCTest
+@testable import mojo_test
+
+class mojo_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
