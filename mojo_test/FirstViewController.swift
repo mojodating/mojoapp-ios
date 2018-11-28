5bb8da2729aa6d490019d8968a04750fc33af4ce
Yunyun Chen <yunyun.chen@consensys.net>
2018-11-28 01:38:45 -0500

    Initial Commit

---
+++ mojo_test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  mojo_test
+//
+//  Created by Yunyun Chen on 11/28/18.
+//  Copyright © 2018 Yunyun Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ mojo_test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  mojo_test
+//
+//  Created by Yunyun Chen on 11/28/18.
+//  Copyright © 2018 Yunyun Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
