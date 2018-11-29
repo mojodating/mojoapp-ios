35862064f92ec4cb19e6c4567423d5f44f770a11
Yunyun Chen <yunyun.chen@consensys.net>
2018-11-29 16:23:34 -0500

    First commit

--- mojo_test/FirstViewController.swift
+++ mojo_test/FirstViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    //MARK: properties
+    @IBOutlet weak var profileImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        profileImage.isUserInteractionEnabled = true
+        
+        let swiftUp = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGesture))
+        swiftUp.direction = UISwipeGestureRecognizer.Direction.up
+        profileImage.addGestureRecognizer(swiftUp)
     }
+    
+    //MARK: actions
 
-
+    @objc func swipeGesture(sendr:UISwipeGestureRecognizer) {
+        if let swipeGesture = sendr as? UISwipeGestureRecognizer
+        {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.up:
+                print("Swipe Up")
+                profileImage.image = UIImage(named:"2")
+            default:
+                break
+            }
+        }
+    }
 }
 
