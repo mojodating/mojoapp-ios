e2810586bdca167da9edf477ed5fc66b71145a09
Tim Siwula <tcsiwula@gmail.com>
2018-12-02 21:10:33 -0600

    latest

--- mojo_test/AppDelegate.swift
+++ mojo_test/AppDelegate.swift
@@ -28,20 +28,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         // database setup
-        ref = Database.database().reference()
-        FirebaseApp.configure()
+        //ref = Database.database().reference()
+//        FirebaseApp.configure()
 
-        let user = Auth.auth().currentUser
-        if let user = user {
-            // The user's ID, unique to the Firebase project.
-            // Do NOT use this value to authenticate with your backend server,
-            // if you have one. Use getTokenWithCompletion:completion: instead.
-            let uid = user.uid
-            let email = user.email
-            let photoURL = user.photoURL
-            // ...
-        }
-        
+//        let user = Auth.auth().currentUser
+//        if let user = user {
+//            // The user's ID, unique to the Firebase project.
+//            // Do NOT use this value to authenticate with your backend server,
+//            // if you have one. Use getTokenWithCompletion:completion: instead.
+//            let uid = user.uid
+//            let email = user.email
+//            let photoURL = user.photoURL
+//            // ...
+//        }
+//
+//        self.ref.child("users/\(user?.uid)/name").setValue("frank")
+//
+//        self.ref.child("users").child("tim").setValue(["name": "frank"])
+//
 //        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
 //        changeRequest?.displayName = displayName
 //        changeRequest?.commitChanges { (error) in
---
+++ mojo_test/Controllers/FetchUsersController.swift
@@ -0,0 +1,9 @@
+//
+//  FetchUsersController.swift
+//  mojo_test
+//
+//  Created by tim.siwula on 12/2/18.
+//  Copyright © 2018 Yunyun Chen. All rights reserved.
+//
+
+import Foundation
