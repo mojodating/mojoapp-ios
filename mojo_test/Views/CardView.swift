579bba24c0e652447e832ae3fe3c516f8947e7ed
Yunyun Chen <yunyun.chen@consensys.net>
2018-12-02 19:51:54 -0500

    add swiping animation - swipe right

--- mojo_test/Views/CardView.swift
+++ mojo_test/Views/CardView.swift
@@ -12,6 +12,9 @@ class CardView: UIView {
     
     fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "5"))
     
+    //configuration
+    fileprivate let threshold: CGFloat = 100
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         //custom code
@@ -26,34 +29,41 @@ class CardView: UIView {
     
     @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
+        
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            
+            handleEnded(gesture)
+        default:
+            ()
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
         let degrees: CGFloat = translation.x / 20
         let angle = degrees * .pi / 180
         
         let rotationalTransformation = CGAffineTransform (rotationAngle: angle)
         self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        let shouldDismissCard = gesture.translation(in: nil).x > threshold
         
-        switch gesture.state {
-        case .changed:
-            let translation = gesture.translation(in: nil)
-            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
-        case .ended:
-            
-            let shouldDismissCard = true
-            
-            UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1,initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6,initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            if shouldDismissCard {
                 
-                if shouldDismissCard {
-                    
-                    let offScreenTransform = self.transform.translatedBy(x: 1000, y: 0)
-                    
-                    self.transform = offScreenTransform
-                } else {
-                   self.transform = .identity
-                }
-            }) {(_) in                
+                self.frame = CGRect (x: 1000 , y: 0, width: self.frame.width, height: self.frame.height)
+            } else {
+                self.transform = .identity
             }
-        default:
-            ()
+        }) {(_) in
+//            print("completed, bring back")
+                self.transform = .identity
+            self.frame = CGRect (x: 0 , y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
         }
     }
     
